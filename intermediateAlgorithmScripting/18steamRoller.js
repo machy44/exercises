fd7e95645b54a1e1db7963ae4e944629120cfcfe
Alen Sljivar <machy44@gmail.com>
2017-11-19 23:34:10 +0100

    steam roller to finish and task foor binary agents challenge

--- intermediateAlgorithmScripting/18steamRoller.js
+++ intermediateAlgorithmScripting/18steamRoller.js
@@ -8,9 +8,34 @@ steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4]
 */
 function steamrollArray(arr) {
   // I'm a steamroller, baby
-  return arr;
+  console.log('arr ulazak u funkciju: ', arr);
+  var newArr = [];
+  var i = 0;
+  for (; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) == false) {
+      newArr.push(arr[i]);
+      console.log('arr[i] u prvom foru: ', arr[i]);
+      console.log('newArr u prvom foru ', newArr);
+    } else {
+      console.log('u elseu:------------------------------');
+      console.log('arr[i] prije drugog fora: ', arr[i]);
+      console.log('newArr prije drugog fora ', newArr);
+      for (var j = 0; j < arr[i].length; j++) {
+        console.log('arr[i][j] u drugom foru: ', arr[i][j]);
+        newArr.push(arr[i][j]);
+        console.log('newArr nakon pusha: ', newArr);
+        console.log('arr nakon newArr pusha: ', arr);
+        if (Array.isArray(arr[i][j]) === true) {
+          steamrollArray(newArr);
+        }
+      };
+    }
+  }
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+//console.log('\n'),
+//console.log(steamrollArray([1, [], [3, [[4]]]]));
 //Other solution
 //https://forum.freecodecamp.org/t/freecodecamp-algorithm-challenge-guide-steamroller/16079
