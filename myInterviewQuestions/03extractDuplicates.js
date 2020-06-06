6a4db50dfdaef3cbcf2ecef875a4331ea4d98986
machy44 <machy44@gmail.com>
2020-06-06 13:07:28 +0200

    update extract duplicates

--- myInterviewQuestions/03extractDuplicates.js
+++ myInterviewQuestions/03extractDuplicates.js
@@ -3,13 +3,9 @@ let array = [1, 2, 3, 4, 4, 5, 2, 9, 7, 8, 8];
 
 const extractDuplicates = (param) => {
   let lookup = {};
-  param.forEach((number) => {
-    if (lookup[number]) {
-      lookup[number] += 1;
-    } else {
-      lookup[number] = 1;
-    }
-  });
+  param.forEach((number) =>
+    lookup[number] ? (lookup[number] += 1) : (lookup[number] = 1)
+  );
   return Object.keys(lookup).filter((key) => lookup[key] > 1);
 };
 
