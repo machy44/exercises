adcd1a134f8d034cd231e785e9b1c8f4272432a0
machy44 <machy44@gmail.com>
2020-06-21 23:35:10 +0200

    solution for repeated subbstring pattern

--- leetCodeTasks/02repeatedSubstringPattern.js
+++ leetCodeTasks/02repeatedSubstringPattern.js
@@ -1,7 +1,21 @@
 // https://leetcode.com/problems/repeated-substring-pattern/
 
 /**
- * @param {string} s
+ * @param {string}
  * @return {boolean}
  */
-var repeatedSubstringPattern = function (s) {};
+
+var repeatedSubstringPattern = function (s) {
+  let i = 1,
+    len = s.length;
+  while (i <= Math.floor(len / 2)) {
+    if (s.slice(0, i).repeat(len / i) == s) return true;
+    i++;
+  }
+  return false;
+};
+
+console.log(repeatedSubstringPattern("abab"));
+console.log(repeatedSubstringPattern("aba"));
+console.log(repeatedSubstringPattern("abcabcabcabc"));
+console.log(repeatedSubstringPattern("ababab"));
