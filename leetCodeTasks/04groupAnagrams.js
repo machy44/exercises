3b22c1323d4c8ba6fdeb2a59416d00e55582fa83
machy44 <machy44@gmail.com>
2020-07-28 20:46:06 +0200

    [WIP] groupAnagrams and validPalindroms leetcode chalenges

---
+++ leetCodeTasks/04groupAnagrams.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/group-anagrams/
+
+/**
+ * @param {string[]}
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  let vowels = ["a", "e", "i", "o", "u"];
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
---
+++ leetCodeTasks/05validPalindrome.js
@@ -0,0 +1,7 @@
+//leetcode.com/problems/valid-palindrome/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+https: var isPalindrome = function (s) {};
