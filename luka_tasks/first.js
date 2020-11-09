f19733b490d2bb887c6f02f9ee70558b01a31daf
alen.sljivar <alen.sljivar@five.agency>
2020-11-09 14:45:11 +0100

    add luka_tasks folder which will have solution algorithm and diagrams which I have to draw

---
+++ luka_tasks/first.js
@@ -0,0 +1,3 @@
+// jednostavan algoritam za slaganje stringova unazad i napravi pseudokod
+
+// dijagram, uzmi react router ...znaci nacrtaj block dijagram komponenti i njihovih veza i mozes flow dijagram. znaci to mogu biti 3 skice
