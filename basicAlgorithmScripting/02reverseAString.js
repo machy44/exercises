8f9f70d7e0cf4ca2b989019aa449e5bea24c57fd
Alen Sljivar <7934791+machy44@users.noreply.github.com>
2020-11-09 12:40:31 +0100

    add reverseAString

---
+++ basicAlgorithmScripting/02reverseAString.js
@@ -0,0 +1,7 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/reverse-a-string
+
+function reverseString(str) {
+  return str;
+}
+
+console.log(reverseString("hello"));
