6dc2109e4b1b272785229eaa93ca92dc6b5f6ef1
alen.sljivar <alen.sljivar@five.agency>
2020-11-09 14:44:20 +0100

    add solution to reverseAString

--- basicAlgorithmScripting/02reverseAString.js
+++ basicAlgorithmScripting/02reverseAString.js
@@ -1,7 +1,14 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/reverse-a-string
 
 function reverseString(str) {
-  return str;
+  let reversed = "";
+  for (let i in str) {
+    if (!reversed) reversed = reversed.concat(str[i]);
+    else {
+      reversed = str[i] + reversed;
+    }
+  }
+  return reversed;
 }
 
 console.log(reverseString("hello"));
