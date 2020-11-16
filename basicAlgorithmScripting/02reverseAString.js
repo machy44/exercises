00b1e59908e5f25492ddfaf2bddb36c64c11bf12
alen.sljivar <alen.sljivar@five.agency>
2020-11-16 11:11:30 +0100

    add solutions for luka task

--- basicAlgorithmScripting/02reverseAString.js
+++ basicAlgorithmScripting/02reverseAString.js
@@ -12,3 +12,5 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello"));
+
+// https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
--- luka_tasks/first.js
+++ luka_tasks/first.js
@@ -1,3 +1,23 @@
 // jednostavan algoritam za slaganje stringova unazad i napravi pseudokod
 
 // dijagram, uzmi react router ...znaci nacrtaj block dijagram komponenti i njihovih veza i mozes flow dijagram. znaci to mogu biti 3 skice
+function reverseString(string) {
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
+}
+
+console.log(reverseString("nesto"));
+
+// recursion
+// pseudokod
+//  inverseString()
+//  if there is no character left return "" to abort recursion call
+//  take last character + call inverseString(string without last character)
+function inverseString(str) {
+  if (!str) return "";
+  return str[str.length - 1] + inverseString(str.slice(0, str.length - 1));
+}
+console.log(inverseString("nesto"));
