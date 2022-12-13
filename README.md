df8ad329b2d6aabc8f3ee54421bcd4a4ca7f28c8
alen.sljivar <alen.sljivar@five.agency>
2022-12-13 11:32:59 +0100

    resolve romanToInteger

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Problems from free code camp challenges, leetcode and interview questions
 
 https://www.freecodecamp.org/
+
 https://leetcode.com/
---
+++ leetCodeTasks/08romanToInteger.js
@@ -0,0 +1,46 @@
+const symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+const subtraction = (first, second) => {
+  if (second - first > 1 && second - first < 9) {
+    return second - 1;
+  }
+  if (second - first > 9 && second - first < 100) {
+    return second - 10;
+  }
+  if (second - first > 100 && second - first < 1000) {
+    return second - 100;
+  }
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const romanToInt = function (s) {
+  let result = 0;
+
+  for (let index = 0; index < s.length; index++) {
+    const element = s[index];
+    const nextElement = s[index + 1];
+
+    if (symbols[element] < symbols[nextElement]) {
+      result = result + subtraction(symbols[element], symbols[nextElement]);
+      index++;
+    } else {
+      result = result + symbols[element];
+    }
+  }
+  return result;
+};
+
+// console.log(romanToInt("III"));
+// console.log(romanToInt("LVIII"));
+console.log(romanToInt('MCMXCIV')); // 1994
