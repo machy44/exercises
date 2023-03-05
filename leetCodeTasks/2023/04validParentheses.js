c14ae652bbd39c3d934005bc114d2e520c739054
machy44 <machy44@gmail.com>
2023-03-05 19:30:16 +0100

    finish valid parentheses

--- leetCodeTasks/2023/04validParentheses.js
+++ leetCodeTasks/2023/04validParentheses.js
@@ -4,4 +4,29 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+
+const validPairs = ['()', '{}', '[]'];
+var isValid = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const element = s[i];
+    if (!stack.length) {
+      stack.push(element);
+      continue;
+    }
+    const prevElement = stack[stack.length - 1];
+    if (pairs.includes(`${prevElement}${element}`)) {
+      stack.pop();
+    } else {
+      stack.push(element);
+    }
+  }
+
+  return stack.length ? false : true;
+};
+
+console.log(isValid('()'));
+console.log(isValid('()[]{}'));
+console.log(isValid('(]'));
+console.log(isValid('[()]{}')); // valid
+// console.log(isValid('([]{})')); // valid
