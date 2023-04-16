cd876b85e58bf7b7193724a6712fa4d2fe20d5bb
machy44 <machy44@gmail.com>
2023-04-16 18:00:00 +0200

    solution for searchInsertPosition

---
+++ leetCodeTasks/2023/09searchInsertPosition.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/search-insert-position/
+// binary search
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// this works but take a look about binary search
+var searchInsert = function (nums, target) {
+  let insertIndex = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    if (element >= target) {
+      insertIndex = i;
+      break;
+    }
+  }
+  return insertIndex;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5)); // 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // 4
