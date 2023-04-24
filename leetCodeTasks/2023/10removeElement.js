9a0056f10cbb48382df94a3074431faa4ac67df8
machy44 <machy44@gmail.com>
2023-04-24 22:40:12 +0200

    remove element solution

--- leetCodeTasks/2023/10removeElement.js
+++ leetCodeTasks/2023/10removeElement.js
@@ -5,4 +5,17 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function (nums, val) {};
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; ) {
+    const element = nums[i];
+    if (element === val) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return nums.length;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3)); //2
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5;
