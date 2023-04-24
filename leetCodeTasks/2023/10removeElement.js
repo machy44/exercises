ae79317e751ecdbe63f825323aa21d5ca050665f
machy44 <machy44@gmail.com>
2023-04-24 22:43:57 +0200

    add comments

--- leetCodeTasks/2023/10removeElement.js
+++ leetCodeTasks/2023/10removeElement.js
@@ -19,3 +19,8 @@ var removeElement = function (nums, val) {
 
 console.log(removeElement([3, 2, 2, 3], 3)); //2
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5;
+
+// i like this one without splice https://leetcode.com/problems/remove-element/solutions/430881/this-is-how-you-solve-all-in-pla
+
+// I believe this is the right approach. Without splice but with two pointers
+// https://leetcode.com/problems/remove-element/solutions/1189866/javascript-solution-two-pointers/
