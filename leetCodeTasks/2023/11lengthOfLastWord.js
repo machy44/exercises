cfa1f69eed7d515edc9998e0b85e3d18ff7e06d2
machy44 <machy44@gmail.com>
2023-04-29 16:17:22 +0200

    add comment to 11lengthOfLastWord.js

--- leetCodeTasks/2023/11lengthOfLastWord.js
+++ leetCodeTasks/2023/11lengthOfLastWord.js
@@ -22,3 +22,21 @@ console.log(lengthOfLastWord('Hello World')); // 5
 console.log(lengthOfLastWord('   fly me   to   the moon  ')); // 4
 console.log(lengthOfLastWord('luffy is still joyboy')); // 6
 console.log(lengthOfLastWord('a')); // 1
+
+// I like this solution also because it uses while loop
+//https://leetcode.com/problems/length-of-last-word/solutions/2875428/javascript-multiple-approaches-easy-to-understand/
+// var lengthOfLastWord = function(s) {
+
+//     let index = s.length - 1;
+//     let output = 0;
+
+//     while (s[index] === ' ' && index >= 0) index -= 1;
+
+//     while (s[index] !== ' ' && index >= 0) {
+
+//         output += 1;
+//         index -= 1;
+
+//     }
+//     return output;
+// };
