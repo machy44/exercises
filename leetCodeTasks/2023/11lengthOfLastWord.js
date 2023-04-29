bfb5eb2ea15283a22972f230a4d2d13ce0d84b69
machy44 <machy44@gmail.com>
2023-04-29 16:08:49 +0200

    resolve 11lengthOfLastWord

--- leetCodeTasks/2023/11lengthOfLastWord.js
+++ leetCodeTasks/2023/11lengthOfLastWord.js
@@ -4,4 +4,21 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function (s) {};
+var lengthOfLastWord = function (s) {
+  if (!s.length) return 0;
+  let wordLength = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === ' ' && wordLength !== 0) {
+      break;
+    }
+    if (s[i] !== ' ') {
+      wordLength += 1;
+    }
+  }
+  return wordLength;
+};
+
+console.log(lengthOfLastWord('Hello World')); // 5
+console.log(lengthOfLastWord('   fly me   to   the moon  ')); // 4
+console.log(lengthOfLastWord('luffy is still joyboy')); // 6
+console.log(lengthOfLastWord('a')); // 1
