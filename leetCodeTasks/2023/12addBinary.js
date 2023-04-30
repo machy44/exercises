3f05be14524244b2a4dbf2225e2f2aa24c21ca2e
machy44 <machy44@gmail.com>
2023-04-30 16:24:32 +0200

    created unsatisfied answer for adding the binary

--- leetCodeTasks/2023/12addBinary.js
+++ leetCodeTasks/2023/12addBinary.js
@@ -5,4 +5,63 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function (a, b) {};
+// submitted answer but I dont like it. too lengthy and mutating the input
+var addBinary = function (a, b) {
+  const equilizeLength = () => {
+    if (a.length > b.length) {
+      const diff = a.length - b.length;
+      for (let index = 0; index < diff; index++) {
+        b = '0' + b;
+      }
+    } else if (b.length > a.length) {
+      const diff = b.length - a.length;
+      for (let index = 0; index < diff; index++) {
+        a = '0' + a;
+      }
+    }
+  };
+  equilizeLength();
+  let res = '';
+  let carryOver = '0';
+  for (let i = a.length - 1; i >= 0; i--) {
+    if (carryOver === '0') {
+      if (a[i] === '1' && b[i] === '1') {
+        res = '0' + res;
+        carryOver = '1';
+        continue;
+      }
+      if ((a[i] === '1' && b[i] === '0') || (a[i] === '0' && b[i] === '1')) {
+        res = '1' + res;
+        carryOver = '0';
+        continue;
+      }
+      if (a[i] === '0' && b[i] === '0') {
+        res = '0' + res;
+        carryOver = '0';
+        continue;
+      }
+    }
+
+    if (carryOver === '1') {
+      if (a[i] === '1' && b[i] === '1') {
+        res = '1' + res;
+        carryOver = '1';
+        continue;
+      }
+      if ((a[i] === '1' && b[i] === '0') || (a[i] === '0' && b[i] === '1')) {
+        res = '0' + res;
+        carryOver = '1';
+        continue;
+      }
+      if (a[i] === '0' && b[i] === '0') {
+        res = '1' + res;
+        carryOver = '0';
+        continue;
+      }
+    }
+  }
+  return carryOver === '1' ? carryOver + res : res;
+};
+
+console.log(addBinary('11', '1')); // 100
+console.log(addBinary('1010', '1011')); // 10101
