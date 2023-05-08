538ff3fb41d52d43be04607262312d542ba618da
machy44 <machy44@gmail.com>
2023-05-08 23:09:07 +0200

    new solution without sorted array

--- leetCodeTasks/2023/05mergeTwoSortedLists.js
+++ leetCodeTasks/2023/05mergeTwoSortedLists.js
@@ -55,17 +55,39 @@ var mergeTwoSortedLists = function (list1, list2) {
   return newList;
 };
 
+// second way without currents and sorted array
+var mergeTwoLists = function (list1, list2) {
+  let newList = { val: -1, next: {} };
+  let current = newList;
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return newList.next;
+};
+
+// recursion way
+
 // const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 // const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
-const list1 = new ListNode(5, new ListNode(10, new ListNode(15)));
-// const list1 = new ListNode(5, new ListNode(10));
-// const list2 = new ListNode(2, new ListNode(3, new ListNode(20)));
-const list2 = new ListNode(2, new ListNode(3));
+// const list1 = new ListNode(5, new ListNode(10, new ListNode(15)));
+const list1 = new ListNode(5, new ListNode(10));
+const list2 = new ListNode(2, new ListNode(3, new ListNode(20)));
+// const list2 = new ListNode(2, new ListNode(3));
 
 // console.log(mergeTwoSortedLists(new ListNode(), new ListNode(0)));
 // console.log(mergeTwoSortedLists([], [0]));
-console.log(mergeTwoSortedLists(list1, list2));
+console.log(mergeTwoLists(list1, list2));
 
 // other solutions
 
