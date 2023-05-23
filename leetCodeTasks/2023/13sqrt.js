bf74b9b39ae4dfd86966794b678b350b527ac073
machy44 <machy44@gmail.com>
2023-05-23 23:01:54 +0200

    solution for sqrt

--- leetCodeTasks/2023/13sqrt.js
+++ leetCodeTasks/2023/13sqrt.js
@@ -4,4 +4,29 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x) {};
+// how to calculate?
+// 1. prime number factorization
+// 2. long division method
+// 3. repeated substraction - this mthod works only for perfect square numbers.
+var mySqrt = function (x) {
+  var left = 1;
+  var right = Math.floor(x / 2) + 1;
+  var mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+};
+
+console.log(mySqrt(4));
+console.log(mySqrt(8));
