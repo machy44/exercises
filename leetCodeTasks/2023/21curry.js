fba47ab6204fb2b33a55ee459f3e1e170d6bd583
machy44 <machy44@gmail.com>
2023-05-29 13:53:40 +0200

    add curry task

---
+++ leetCodeTasks/2023/21curry.js
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/curry/
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function (fn) {
+  return function curried() {};
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
