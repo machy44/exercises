df47c0adba35f46790b8493a354fa80994a5c490
machy44 <machy44@gmail.com>
2023-05-29 22:44:44 +0200

    add 6 in traversal tree

--- leetCodeTasks/2023/16binaryTreeInorderTraversal.js
+++ leetCodeTasks/2023/16binaryTreeInorderTraversal.js
@@ -60,6 +60,7 @@ binTree.right.left = new TreeNode(3);
 let root = new TreeNode(1);
 root.left = new TreeNode(2);
 root.right = new TreeNode(3);
+root.right.left = new TreeNode(6);
 root.left.left = new TreeNode(4);
 root.left.right = new TreeNode(5);
 
