2140177247ad224c26d505af240598c17e2ebb10
machy44 <machy44@gmail.com>
2023-05-30 19:07:59 +0200

    add design hashSet task

---
+++ leetCodeTasks/2023/23designHashSet.js
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/design-hashset/
+
+// this is example with class which would work in es6 as it works
+//  with typescript
+// class MyHashSet {
+//   constructor() {}
+
+//   add(key: number): void {}
+
+//   remove(key: number): void {}
+
+//   contains(key: number): boolean {}
+// }
+
+var MyHashSet = function () {};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function (key) {};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function (key) {};
+
+/**
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function (key) {};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
