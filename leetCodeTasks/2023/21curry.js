71b9fc5098ab9522d107b5cdd69ddb89e93a515c
machy44 <machy44@gmail.com>
2023-05-30 19:04:43 +0200

    update curry

--- leetCodeTasks/2023/21curry.js
+++ leetCodeTasks/2023/21curry.js
@@ -4,6 +4,8 @@
  * @param {Function} fn
  * @return {Function}
  */
+
+// my solution
 var curry = function (fn) {
   let prevArgs = [];
   return function curried(...args) {
@@ -11,9 +13,22 @@ var curry = function (fn) {
     while (prevArgs.length < fn.length) {
       return curried;
     }
-    return fn(...prevArgs);
+    let result = fn(...prevArgs);
+    prevArgs = [];
+    return result;
   };
 };
+// i like this solution better https://leetcode.com/problems/curry/solutions/3522579/currying-use-case-dry-run-beginner-friendly-o-fn-js-ts-day10/
+// var curry = function (fn) {
+//   return function curried(...args) {
+//     if (args.length < fn.length) {
+//       return function (...args2) {
+//         return curried(...args, ...args2);
+//       };
+//     }
+//     return fn(...args);
+//   };
+// };
 
 function sum(a, b) {
   return a + b;
@@ -27,5 +42,6 @@ function sum2(a, b, c) {
 
 const csum2 = curry(sum2);
 // console.log(csum2(1, 2)(3));
+console.log(csum2(1)(2)(3));
 
 console.log(csum2()()(1, 2, 3));
