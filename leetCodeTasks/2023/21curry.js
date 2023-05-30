bc3cd748d80179aa6dcce1bd74573f428430824b
machy44 <machy44@gmail.com>
2023-05-30 18:55:11 +0200

    solution for curry

--- leetCodeTasks/2023/21curry.js
+++ leetCodeTasks/2023/21curry.js
@@ -5,11 +5,27 @@
  * @return {Function}
  */
 var curry = function (fn) {
-  return function curried() {};
+  let prevArgs = [];
+  return function curried(...args) {
+    prevArgs = [...prevArgs, ...args];
+    while (prevArgs.length < fn.length) {
+      return curried;
+    }
+    return fn(...prevArgs);
+  };
 };
 
-/**
- * function sum(a, b) { return a + b; }
- * const csum = curry(sum);
- * csum(1)(2) // 3
- */
+function sum(a, b) {
+  return a + b;
+}
+
+function sum2(a, b, c) {
+  return a + b + c;
+}
+// const csum = curry(sum);
+// console.log(csum(1)(2)); // 3
+
+const csum2 = curry(sum2);
+// console.log(csum2(1, 2)(3));
+
+console.log(csum2()()(1, 2, 3));
