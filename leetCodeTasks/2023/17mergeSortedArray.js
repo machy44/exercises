30ade19411fbf8fffc05616b8c99eb0990c7c8c6
machy44 <machy44@gmail.com>
2023-06-04 22:22:09 +0200

    solution for mergeSortedArray

--- leetCodeTasks/2023/17mergeSortedArray.js
+++ leetCodeTasks/2023/17mergeSortedArray.js
@@ -8,17 +8,24 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  let looping = m >= n ? m : n;
-  for (let i = 0; i < looping; i++) {
-    if (nums2[i] <= nums1[i]) {
-      nums1.splice(i, 0, nums2[i]);
-    } else if (nums1[i] <= nums2[i]) {
-      nums1.splice(i + 1, 0, nums2[i]);
+  let rightPointer = n - 1;
+  let leftPointer = m - 1;
+  let currentPosition = m + n - 1;
+  while (currentPosition >= 0) {
+    if (nums2[rightPointer] >= nums1[leftPointer] || leftPointer < 0) {
+      nums1[currentPosition] = nums2[rightPointer];
+      rightPointer--;
+    } else if (nums1[leftPointer] > nums2[rightPointer]) {
+      nums1[currentPosition] = nums1[leftPointer];
+      nums1[leftPointer] = 0;
+      leftPointer--;
     }
+    currentPosition--;
   }
-  console.log(nums1);
+  return nums1;
 };
 
-merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
-// merge([1], 1, [], 0);
-// merge([], 0, [1], 1);
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(merge([2, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3));
+console.log(merge([1], 1, [], 0));
+console.log(merge([0], 0, [1], 1));
