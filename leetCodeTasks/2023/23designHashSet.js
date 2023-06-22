0161e85ac74daaf4ca4bb262b15d867d19a3b4d8
machy44 <machy44@gmail.com>
2023-06-22 21:21:27 +0200

    solve 23designHashSet

--- leetCodeTasks/2023/23designHashSet.js
+++ leetCodeTasks/2023/23designHashSet.js
@@ -12,25 +12,62 @@
 //   contains(key: number): boolean {}
 // }
 
-var MyHashSet = function () {};
+var MyHashSet = function () {
+  this.data = [];
+};
 
 /**
  * @param {number} key
  * @return {void}
  */
-MyHashSet.prototype.add = function (key) {};
+MyHashSet.prototype.add = function (key) {
+  for (let i = 0; i < this.data.length; i++) {
+    if (key === this.data[i]) {
+      return;
+    }
+  }
+  this.data.push(key);
+};
 
 /**
  * @param {number} key
  * @return {void}
  */
-MyHashSet.prototype.remove = function (key) {};
+MyHashSet.prototype.remove = function (key) {
+  for (let i = 0; i < this.data.length; i++) {
+    if (key === this.data[i]) {
+      this.data.splice(i, 1);
+    }
+  }
+};
 
 /**
  * @param {number} key
  * @return {boolean}
  */
-MyHashSet.prototype.contains = function (key) {};
+MyHashSet.prototype.contains = function (key) {
+  for (let i = 0; i < this.data.length; i++) {
+    if (key === this.data[i]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+MyHashSet.prototype.display = function (key) {
+  console.log(this.data);
+};
+
+const bla = new MyHashSet();
+bla.add(2);
+bla.add(2);
+bla.add(3);
+bla.add(3);
+bla.contains(3);
+bla.display();
+bla.remove(3);
+bla.contains(3);
+bla.display();
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
