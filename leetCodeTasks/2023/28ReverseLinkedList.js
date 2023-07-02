7ce8ac8882e32c168489d66e81230dd75ac9d528
machy44 <machy44@gmail.com>
2023-07-02 23:07:51 +0200

    add task for revereLinkedList

---
+++ leetCodeTasks/2023/28ReverseLinkedList.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {};
