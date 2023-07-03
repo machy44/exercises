8b6a8a7d0df400b72b932506c7b55c7317f92d2e
machy44 <machy44@gmail.com>
2023-07-03 22:37:14 +0200

    solution for 28reverseLinkedList

--- leetCodeTasks/2023/28ReverseLinkedList.js
+++ leetCodeTasks/2023/28ReverseLinkedList.js
@@ -1,14 +1,30 @@
 // https://leetcode.com/problems/reverse-linked-list/
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {};
+var reverseList = function (head) {
+  let stack = [];
+  let reversedList = new ListNode(-1);
+  let dummyHead = reversedList;
+  while (head) {
+    stack.push(head.val);
+    head = head.next;
+  }
+  for (let i = stack.length - 1; i >= 0; i--) {
+    dummyHead.next = new ListNode(stack[i]);
+    dummyHead = dummyHead.next;
+  }
+
+  return reversedList.next;
+};
+
+const list = new ListNode(1, new ListNode(2, new ListNode(3)));
+
+console.log(reverseList(list));
