290ef66ab30fe0893325877a3366095e307445d2
machy44 <machy44@gmail.com>
2023-07-05 21:51:20 +0200

    resolve nextDay task

--- leetCodeTasks/2023/29nextDay.js
+++ leetCodeTasks/2023/29nextDay.js
@@ -1,8 +1,9 @@
 // https://leetcode.com/problems/next-day/
 
-Date.prototype.nextDay = function () {};
+Date.prototype.nextDay = function () {
+  let date = new Date(this.valueOf() + 86400000);
+  return date.toISOString().split('T')[0];
+};
 
-/**
- * const date = new Date("2014-06-20");
- * date.nextDay(); // "2014-06-21"
- */
+const date = new Date('2014-06-20');
+console.log(date.nextDay()); // "2014-06-21"
