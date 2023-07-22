0520b8541032c8701d877881b0cc305b1f8aa15e
machy44 <machy44@gmail.com>
2023-07-22 18:58:15 +0200

    solution for fizzBuzz

--- leetCodeTasks/2023/high-frequency-on-real-interviews/32fizzBuzz.js
+++ leetCodeTasks/2023/high-frequency-on-real-interviews/32fizzBuzz.js
@@ -1,1 +1,24 @@
 // https://leetcode.com/problems/fizz-buzz/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  let res = [];
+  for (let i = 1; i <= n; i++) {
+    let text = '';
+    if (i % 3 === 0) {
+      text = 'Fizz';
+    }
+    if (i % 5 === 0) {
+      text += 'Buzz';
+    }
+    res.push(text || String(i));
+  }
+  return res;
+};
+
+// console.log(fizzBuzz(3));
+// console.log(fizzBuzz(5));
+console.log(fizzBuzz(15));
