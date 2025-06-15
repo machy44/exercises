e28b162c6d6d9947ae388086a2f2b2a6ddf4c6a0
machy44 <machy44@gmail.com>
2025-06-15 18:39:36 +0200

    timeout cancellation

--- leetCodeTasks/2025/difficulty-easy-high-acceptance/timeoutCancellation.js
+++ leetCodeTasks/2025/difficulty-easy-high-acceptance/timeoutCancellation.js
@@ -6,30 +6,51 @@
  * @param {number} t
  * @return {Function}
  */
+
+// const cancellable = function (fn, args, t) {
+
+//   const timer = setTimeout(() => {
+//     console.log(fn(...args));
+//   }, t);
+
+//   const cancelFn = function () {
+//     clearTimeout(timer);
+//   };
+
+//   return cancelFn;
+// };
+
+
+// const cancelFn = cancellable((x) => x * 5, [2]);
+// console.log(setTimeout(cancelFn, 50));
+// console.log(cancelFn())
+
+
+// timeout has also third parameter 
+
 var cancellable = function(fn, args, t) {
-    
+  const timerId = setTimeout(fn, t, ...args);
+  const cancelFn = () => {
+      clearTimeout(timerId);
+  }
+  return cancelFn;
 };
 
-/**
- *  const result = [];
- *
- *  const fn = (x) => x * 5;
- *  const args = [2], t = 20, cancelTimeMs = 50;
- *
- *  const start = performance.now();
- *
- *  const log = (...argsArr) => {
- *      const diff = Math.floor(performance.now() - start);
- *      result.push({"time": diff, "returned": fn(...argsArr)});
- *  }
- *       
- *  const cancel = cancellable(log, args, t);
- *
- *  const maxT = Math.max(t, cancelTimeMs);
- *           
- *  setTimeout(cancel, cancelTimeMs);
- *
- *  setTimeout(() => {
- *      console.log(result); // [{"time":20,"returned":10}]
- *  }, maxT + 15)
- */
+const cancelFn = cancellable((x) => x * 5, [2], 20);
+
+setTimeout(cancelFn, 50);
+
+
+// 3rd solutiuon /** with boolean flag
+
+// var cancellable = function(fn, args, t) {
+//  let isCancelled = false;
+//  setTimeout(() => {
+//      if(!isCancelled)
+//          fn(...args);
+//  }, t);
+
+//  return () => {
+//    isCancelled = true;
+//  };
+// }
