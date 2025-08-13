28f46c85c19ea2f124ac8c8f07b3860d5bcbb0c5
machy44 <machy44@gmail.com>
2025-08-13 17:03:04 +0200

    resolve chunkArray task

--- leetCodeTasks/2025/difficulty-easy-high-acceptance/chunkArray.js
+++ leetCodeTasks/2025/difficulty-easy-high-acceptance/chunkArray.js
@@ -12,7 +12,7 @@ var chunk = function (arr, size) {
   for (let i = 0; i < arr.length; i += size) {
     const temp = [];
     for (let j = 0; j < size; j++) {
-      if (arr[i + j]) {
+      if (arr[i + j] !== undefined) {
         temp.push(arr[i + j]);
       }
     }
@@ -26,4 +26,20 @@ var chunk = function (arr, size) {
 // console.log(chunk([1,2,3,4,5], 1));
 // [[1],[2],[3],[4],[5]]
 // console.log(chunk([1, 9, 6, 3, 2], 3)); // [[1,9,6],[3,2]]
-console.log(chunk([8,5,3,2,6], 6)); //[[8,5,3,2,6]]
+// console.log(chunk([8,5,3,2,6], 6)); //[[8,5,3,2,6]]
+console.log(chunk([42,10,19,6,8,7,9,325,0,428,23,757], 3))
+
+// isto dobra ideja je koristiti slice i svidja mi se ovo rjesenje 
+// vise nego moje
+
+// var chunk = function(arr, size) {
+//   var chunkedArray = [];
+//   var index = 0;
+
+//   while (index < arr.length) {
+//     chunkedArray.push(arr.slice(index, index + size));
+//     index += size;
+//   }
+
+//   return chunkedArray;
+// }
