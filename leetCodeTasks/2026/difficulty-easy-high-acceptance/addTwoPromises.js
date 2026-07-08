b47aa615749090434d3b6d8cad25c2f3200ac84f
machy44 <machy44@gmail.com>
2026-07-08 22:09:30 +0200

    playing with new Promise

--- leetCodeTasks/2026/difficulty-easy-high-acceptance/addTwoPromises.js
+++ leetCodeTasks/2026/difficulty-easy-high-acceptance/addTwoPromises.js
@@ -5,12 +5,27 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function (promise1, promise2) {
-  const res = await promise1;
-  const res2 = await promise2;
+// var addTwoPromises = async function (promise1, promise2) {
+//   const res = await promise1;
+//   const res2 = await promise2;
 
-  return Promise.resolve(res + res2);
-};
+//   return res + res2;
+// };
+
+var addTwoPromises = (promise1, promise2) => new Promise((resolve, reject) => {
+    let sum = 0;
+    let count = 0;
+
+    const onResolved = (value) => {
+      sum += value;
+      count++;
+      if (count === 2) resolve(sum);
+    };
+
+    promise1.then(onResolved).catch(reject);
+    promise2.then(onResolved).catch(reject);
+})
+
 
 
 addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
