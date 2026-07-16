c9bfd8fc4393b7320a60c9ae5a3507c216334a70
machy44 <machy44@gmail.com>
2026-07-16 10:30:23 +0200

    max depth

---
+++ leetCodeTasks/2026/difficulty-easy-high-acceptance/maximumDepthBinaryTree.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    
+  if(root === null) return 0;
+  let leftSide = maxDepth(root.left);
+  let rightSide = maxDepth(root.right);
+
+  return 1 + Math.max(leftSide, rightSide);
+};
+
+console.log(maxDepth({
+  val: 3,
+  left: {
+    val: 9,
+    left: null,
+    right: null
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15,
+      left: null,
+      right: null
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null
+    }
+  }
+}));
+// console.log(maxDepth({ val: 5, left: null, right: null }));
+// console.log(maxDepth([1,null,2]))
