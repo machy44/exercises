4235abb1ad3a54d9e5f72e245aa4bd7ebb3e5e1d
machy44 <machy44@gmail.com>
2026-07-19 22:05:59 +0200

    sortedArrayBST solution

--- leetCodeTasks/2026/difficulty-easy-high-acceptance/sortedArraytoBST.js
+++ leetCodeTasks/2026/difficulty-easy-high-acceptance/sortedArraytoBST.js
@@ -1,1 +1,30 @@
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+
+
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    if (nums.length === 0) return null;
+
+    const midIndex = Math.floor(nums.length / 2);
+    const root = new TreeNode(nums[midIndex]);
+
+    root.left = sortedArrayToBST(nums.slice(0, midIndex));
+    root.right = sortedArrayToBST(nums.slice(midIndex + 1));
+
+    return root;
+};
+
+
+console.log(sortedArrayToBST([-10,-3,0,5,9]));
