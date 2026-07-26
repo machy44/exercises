447843ff4fa23f833ff1eeed0b4b58ab69caa8d5
machy44 <machy44@gmail.com>
2026-07-26 19:46:03 +0200

    resolve merge two sorted lists iteratively

--- leetCodeTasks/2026/linked-list/mergeTwoSortedLists.js
+++ leetCodeTasks/2026/linked-list/mergeTwoSortedLists.js
@@ -12,13 +12,31 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+
+// TODO: recursive way
+// iterative way
 var mergeTwoLists = function (list1, list2) {
-  console.log("🚀 ~ mergeTwoLists ~ list2:", list2);
-  console.log("🚀 ~ mergeTwoLists ~ list1:", list1);
-  // TODO: implement with a dummy head + tail pointer, picking the smaller of list1/list2 each step
   let dummy = new ListNode(0);
+  let tail = dummy;
+  let p1 = list1;
+  let p2 = list2;
+  while (p1 !== null && p2 !== null) {
+    if(p1.val <= p2.val) {
+      tail.next = p1;
+      tail = tail.next;
+      p1 = p1.next
+    } else if(p1.val > p2.val) {
+      tail.next = p2;
+      tail = tail.next;
+      p2 = p2.next;
+    }
+
+  }
+
+  tail.next = p1 !== null ? p1 : p2;
 
   return dummy.next;
+
 };
 
 // --- test setup ---
@@ -46,7 +64,9 @@ function listToArray(head) {
   return arr;
 }
 
+// const list1 = buildList([1, 2]);
+// const list2 = buildList([1, 2]);
 const list1 = buildList([1, 2, 4]);
-const list2 = buildList([1, 3, 4]);
+const list2 = buildList([1, 2, 4]);
 const result = mergeTwoLists(list1, list2);
 console.log(listToArray(result)); // expected: [1, 1, 2, 3, 4, 4]
